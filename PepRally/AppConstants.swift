5651301aebbca322fc6212b181294c43d7e83eee
Jeremy Wang <jer.wang25@gmail.com>
2017-04-17 01:17:14 -0700

    first commit

---
+++ PepRally/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  PepRally
+//
+//  Created by Jeremy Wang on 4/17/17.
+//  Copyright © 2017 PepRally. All rights reserved.
+//
+
+import UIKit
+
+struct ColorCodes {
+    static let ColorPrimaryPurple = UIColor(red: 51.0/255.0, green: 0.0/255.0, blue: 111.0/255.0, alpha: 1.0)
+    static let ColorPrimaryGold = UIColor(red: 232.0/255.0, green: 211.0/255.0, blue: 162.0/255.0, alpha: 1.0)
+    static let ColorPrimaryMetallicGold = UIColor(red: 145.0/255.0, green: 123.0/255.0, blue: 76.0/255.0, alpha: 1.0)
+}
---
+++ PepRally/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  PepRally
+//
+//  Created by Jeremy Wang on 4/16/17.
+//  Copyright © 2017 PepRally. All rights reserved.
+//
+
+import UIKit
+import FacebookCore
+import FacebookLogin
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
